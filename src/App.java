1b1f6eb7ed2a96436dae5fc169defc137ef8393e
Yunlong Li <yunlong.li@csiro.au>
2022-09-04 22:14:05 +0800

    add node by id order

--- src/App.java
+++ src/App.java
@@ -5,16 +5,19 @@ public class App {
     public static void main(String[] args) throws Exception {
         HeroNode node1 = new HeroNode(1, "wo");
         HeroNode node2 = new HeroNode(2, "te");
-        HeroNode node3 = new HeroNode(3, "hh");
-        HeroNode node4 = new HeroNode(4, "tt");
+        HeroNode node3 = new HeroNode(5, "hh");
+        HeroNode node4 = new HeroNode(8, "tt");
+        HeroNode node5 = new HeroNode(4, "sdsd");
+        HeroNode node6 = new HeroNode(7, "sdsd");
         SingleLinkedList sLinkedList = new SingleLinkedList();
         sLinkedList.addNode(node1);
         sLinkedList.addNode(node2);
         sLinkedList.addNode(node3);
         sLinkedList.addNode(node4);
+        sLinkedList.addNodeById(node5);
+        sLinkedList.addNodeById(node6);
 
         sLinkedList.display();
 
-        System.out.println(node1.next);
     }
 }
--- src/linkedlist/SingleLinkedList.java
+++ src/linkedlist/SingleLinkedList.java
@@ -10,6 +10,29 @@ public class SingleLinkedList {
         return head.next == null;
     }
 
+    // 按照id顺序添加node到指定位置
+    // 第一种情况，node需要添加到链表中间
+    // 第二种情况，node添加到链表尾部
+    public void addNodeById(HeroNode node) {
+        HeroNode temp = head;
+        while (true) {
+            if (temp.id < node.id && node.id <temp.next.id) {
+                break;
+            }
+            if (temp.id == node.id) {
+                throw new RuntimeException("This node is already exists");
+            }
+            if (temp.next == null) {
+                break;
+            }
+            temp = temp.next;
+        }
+        node.next = temp.next;
+        temp.next = node;
+    }
+
+
+    // 添加node到尾部
     public void addNode(HeroNode node) {
         // 头结点不能动，需要一个辅助节点
         HeroNode temp = head;
